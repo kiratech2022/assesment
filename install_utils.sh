be52c276a30821960ac0865753308bf0a8a2acf4
kiratech2022 <kiratech2022@yandex.com>
2022-04-19 23:42:22 +0200

    add module molecule-vagrant

--- install_utils.sh
+++ install_utils.sh
@@ -4,8 +4,7 @@ echo '--- Install Vagrant plugins'
 vagrant plugin install vagrant-hostsupdater
 vagrant plugin install vagrant-disksize
 
-#docker-swarm
-pip install jmespath molecule
+pip install jmespath molecule molecule-vagrant
 
 #create ssh key
 ssh-keygen -t rsa -N '' -f ~/.ssh/id_rsa -q
